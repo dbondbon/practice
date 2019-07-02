5b014657a864a1fee722ce4e999554fb3eb39511
尘土 <1669691816@qq.com>
2019-07-02 15:16:18 +0800

    String类的endWith和substring方法

---
+++ src/String/StringDemo.java
@@ -0,0 +1,23 @@
+package String;
+
+/**
+ * @ClassName StringDemo
+ * @Descrcription String基础操作的演示类
+ * @Author zzc
+ * @Date 2019/7/2 15:11
+ * @Version 1.0
+ */
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        String string = "abcdefg";
+
+        //1、string.endsWith("aaa")
+        System.out.println(string.endsWith("abc"));
+        System.out.println(string.endsWith("efg"));
+
+        //2、string.substring(a,b)
+        System.out.println(string.substring(2,4));
+    }
+}
