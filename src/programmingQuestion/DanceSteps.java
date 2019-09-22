6654d8f17b74de49220e142532912e2b6e7eb683
尘土 <1669691816@qq.com>
2019-09-22 12:22:08 +0800

    青蛙跳台阶

---
+++ src/programmingQuestion/DanceSteps.java
@@ -0,0 +1,18 @@
+package programmingQuestion;
+
+/**
+ * @ClassName DanceSteps
+ * @Descrcription 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+ * @Author zzc
+ * @Date 2019/9/22 12:16
+ * @Version 1.0
+ */
+public class DanceSteps {
+    public static int jumpFloor(int target) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(jumpFloor(3));
+    }
+}
